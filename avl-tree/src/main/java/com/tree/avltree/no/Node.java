3e44a3e6fd6c9642e3c9701daea6100cba20918c
Eduardo Schulz <33841225+eduardoschulz@users.noreply.github.com>
2023-03-31 20:18:13 -0300

    initial commit

---
+++ avl-tree/src/main/java/com/tree/avltree/no/Node.java
@@ -0,0 +1,20 @@
+package com.tree.avltree.no;
+
+public class Node {
+    private int valor;
+    private Node left, right;
+
+    public Node (int valor){
+        this.valor = valor;
+    }
+    public Node(int valor, Node left, Node right){
+        this.valor  = valor;
+        this.left = left;
+        this.right = right;
+    }
+    public void evalPos(int valor){
+       valor
+
+
+    }
+}
